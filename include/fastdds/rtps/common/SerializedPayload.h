0ab0a38d0dac0b838f788e86d4b4c26c68e144a1
cferreiragonz <carlosferreira@eprosima.com>
2024-06-07 11:07:41 +0200

    Refs #21121: Avoid cyclic release
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- include/fastdds/rtps/common/SerializedPayload.h
+++ include/fastdds/rtps/common/SerializedPayload.h
@@ -93,8 +93,17 @@ struct FASTDDS_EXPORTED_API SerializedPayload_t
         this->reserve(len);
     }
 
+    /*!
+     * Destructor
+     * A call to \ref release_payload() is expected before the \ref empty() is called if the
+     * payload_owner is not nullptr.
+     */
     ~SerializedPayload_t()
     {
+        if (payload_owner != nullptr)
+        {
+            payload_owner->release_payload(*this);
+        }
         this->empty();
     }
 
@@ -107,7 +116,7 @@ struct FASTDDS_EXPORTED_API SerializedPayload_t
     }
 
     /*!
-     * Copy another structure (including allocating new space for the data.)
+     * Copy another structure (including allocating new space for the data).
      * @param[in] serData Pointer to the structure to copy
      * @param with_limit if true, the function will fail when providing a payload too big
      * @return True if correct
@@ -153,13 +162,12 @@ struct FASTDDS_EXPORTED_API SerializedPayload_t
         return true;
     }
 
-    //! Empty the payload
+    /*!
+     * Empty the payload
+     * @pre payload_owner must be nullptr
+     */
     void empty()
     {
-        if (payload_owner != nullptr)
-        {
-            payload_owner->release_payload(*this);
-        }
         assert(payload_owner == nullptr);
 
         length = 0;
